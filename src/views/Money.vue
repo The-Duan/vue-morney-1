1f3ef4417dcc76d56a4e7709b334cc34ae4cf9ca
The-Duan <1950730613@qq.com>
2022-03-05 10:57:56 +0800

    页面å布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
   </div>
 </template>
 
@@ -11,5 +13,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  flex-grow: 1;
+  overflow: auto;
+}
 </style>
