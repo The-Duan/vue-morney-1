d0e914bcd7d1154c8664a035e9c82d7b22da667b
The-Duan <1950730613@qq.com>
2022-03-05 10:31:24 +0800

    创建404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>你当前页面不存在</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
