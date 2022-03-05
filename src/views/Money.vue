15b46d8dcc067a3b84232f75b246d669968888da
The-Duan <1950730613@qq.com>
2022-03-05 20:44:07 +0800

    é¡导航栏css

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Money.vue
-    </div>
-  </div>
+  <layout>
+    <p>Money.vue</p>
+  </layout>
 </template>
 
 <script lang="ts">
@@ -13,14 +11,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
-  border: 1px solid red;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content{
-  flex-grow: 1;
-  overflow: auto;
-}
+
 </style>
